fed82c04249e872fa66e5ec107bc60f0d12bd05d
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-08-26 16:25:23 -0300

    req01

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs/promises');
 const bodyParser = require('body-parser');
+const { application } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +16,14 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const path = 'src/talker.json';
+  console.log(path);
+  const talkers = JSON.parse(await fs.readFile(path));
+  if (talkers.length > 0) {
+  return res.status(200).json(talkers);
+  }
+  return res.status(200).json([]);
 });
